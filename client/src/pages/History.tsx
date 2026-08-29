512bca781b14c15c27fdb4e2a3ad25aa7bae4309
Anil Suva <suvaanil80@gmail.com>
2026-08-29 14:05:43 +0530

    a little bug in importing a logo has been fixed

--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import PaytmLogo from "../../components/PaytmLogo";
+import PaytmLogo from "../components/PaytmLogo";
 import { FiArrowUpRight, FiArrowDownLeft } from "react-icons/fi";
 
 interface Transaction {
